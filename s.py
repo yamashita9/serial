dad5b9b0acd0391f848da948f2d84b9267e96951
yamashita9 <yakoyako216@gmail.com>
2017-05-21 01:50:14 +0900

    Update s.py

--- s.py
+++ s.py
@@ -1,4 +1,5 @@
-# MIT License
+MIT License
+
 #Copyright (c) 2017 yamashita9
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -7,6 +8,7 @@
 #to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 #copies of the Software, and to permit persons to whom the Software is
 #furnished to do so, subject to the following conditions:
+
 #The above copyright notice and this permission notice shall be included in all
 #copies or substantial portions of the Software.
 
